64a0dcacead8dd8f7013ed17344bd902b81c7665
Oleksandr Zheliabin <oleksandr.zheliabin@gmail.com>
2024-01-10 22:25:20 -0800

    Added math

--- js/math.js
+++ js/math.js
@@ -2,17 +2,35 @@
 //         positive number, make sure to display the positive version of that number in the console window. 
 //         If I enter -15 in the prompt and -15 is displayed in the console window, you did this one wrong.
 
+// x = prompt('Please, enter number:')
+// console.log(Math.abs(x))
+
 //STEP 2 - Create an application that prompts the user for a floating point value (decimal). Store the result of that 
 //         input in a variable and then round it up to the nearest whole number. Display the result within a console 
 //         window.
+
+// x = prompt('Please, enter floating point value (decimal):')
+// console.log(Math.round(x))
 
 //STEP 3 - Create an application that prompts the user for a floating point value (decimal). Store the result of that 
 //         input in a variable and then round it down to the nearest whole number. Display the result within a console 
 //         window.
 
+// x = prompt('Please, enter floating point value (decimal):')
+// console.log(Math.floor(x))
+
 //STEP 4 - Create an application that prompts the user for 5 numbers. Ask them to comma delimit each number so you 
 //         get 1,2,3,4,5 for example. Store the result of that input in a variable and then find the largest and 
 //         smallest numbers in that list. Display both of those numbers within a console window.
 
+// x = prompt('Please, enter 5 numbers, comma delimit (for example 1,2,3,4,5):')
+// let arrX = x.split(',')
+// console.log('Largest number: ' + Math.max(...arrX))
+// console.log('Smallest number: ' + Math.min(...arrX))
+
+
 //STEP 5 - Create an application that prompts the user for a number. Now find the square root of that number and 
 //         display the result within a console window.
+
+// x = prompt('Please, enter number:')
+// console.log(Math.sqrt(x))
